361c0f1f69a0d8ffb80869dbc7a929356543edc4
Makoto Taniwaki <macopy123@gmail.com>
2013-01-28 13:57:50 +0000

    Import of MACOPY/Test-Deep-Between-0.02 from CPAN.
    gitpan-cpan-distribution: Test-Deep-Between
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MACOPY/Test-Deep-Between-0.02.tar.gz
    gitpan-cpan-author:       MACOPY
    gitpan-cpan-maturity:     released

--- .dim.pl
+++
@@ -1,7 +0,0 @@
-# This file is managed by /Users/mackee/perl5/perlbrew/perls/perl-5.14.2/bin/dim.
-{
-  dist => 'Test::Deep::Between',
-  distdir => 'Test-Deep-Between',
-  module => 'Test::Deep::Between',
-  template => 'Default'
-}
